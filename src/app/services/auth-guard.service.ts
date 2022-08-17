c56e0e2642552d033da502dc339b0a7dbad6f714
Alejandro Cerrato <alejandrocerrato@disofic.com>
2022-08-17 09:41:46 +0200

    Guardia finalizada por id de usuario de odoo

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,31 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRoute, CanActivate } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
-  public odooUser: number;
   public allowedUsers: number[] = [2, 61, 62, 78, 63, 82, 102, 109, 108, 73];
 
   constructor(
-    public router: Router,
-    private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
-  canActivate(): Promise<boolean> {
-    return new Promise(resolve => {
-      this.route.queryParams
-      .subscribe(params => {
-        console.log(params);
-        this.odooUser = params['user_odoo'];
-        if (!this.allowedUsers.includes(this.odooUser) || this.odooUser == undefined) {
-          this.router.navigate(['noAccess']);
-          resolve(false);
-        }
-          resolve(true);
-      });
-    })
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const queryParams = route.queryParamMap;
+    const userOdoo = queryParams['params']['user_odoo'];
+    if (!this.allowedUsers.includes(parseInt(userOdoo)) || userOdoo == undefined) {
+      this.router.navigate(['noAccess']);
+      return false;
+    }
+    return true;
   }
 
 }
